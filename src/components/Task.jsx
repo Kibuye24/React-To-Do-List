8a5647b58e83c0d1422c10978e763c7e2467c0e1
Kibuye24 <93917125+Kibuye24@users.noreply.github.com>
2022-11-13 09:58:22 +0300

    Update Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 
-const Job = ( props ) => {
+const Task = ( props ) => {
 
     return (
-        <div className='task'>
-            <h1 className='taskItem' style={{backgroundColor: props.completed ? "green" : "yellow"}}>{ props.taskName }</h1>
-            <button className='taskItemButton'onClick={() => props.deleteTask(props.id)}> X </button>
-            <button className='taskItemDone' style={{content: props.completed ? "Bravo" : "Done"}} onClick={() => props.completeTask(props.id)}> Done </button>
+        <div className='task'
+             style={{backgroundColor: props.completed ? "greenyellow" : "grey"}}
+        >
+            <h1 className='taskItem' style={
+                {backgroundColor: props.completed ? "greenyellow" : "yellow " ,
+                color: props.completed ? "white" : "black",
+                borderStyle: props.completed ? "none" : "solid",
+                opacity: props.completed ? "100%" : "70%"
+                }
+                }>{ props.taskName }</h1>
+            <button className='taskItemButton' onClick={() => props.deleteTask(props.id)}> X </button>
+            <button className='taskItemDone' onClick={() => props.completeTask(props.id)}
+                style={{visibility: props.completed ? "none" : "visible", borderStyle: props.completed ? "none" : "solid"}}
+            > Done </button>
         </div> 
     ) 
 }
 
-export default Job;
+export default Task;
